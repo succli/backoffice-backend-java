01a22ad85c6e9711fa97d9c8623779d3520f1758
Zsolt Schutzbach <succ92@gmail.com>
2021-01-29 20:05:44 +0100

    'Initial'

---
+++ src/test/java/hu/zschutzbach/backoffice/BackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.zschutzbach.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
